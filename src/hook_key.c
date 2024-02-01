504362107af3a2939adcc25409f71b45c5b968cc
minakim <minakim@student.42berlin.de>
2024-02-01 22:55:36 +0100

    add: camera control functions / -ing

--- src/hook_key.c
+++ src/hook_key.c
@@ -6,27 +6,130 @@
 /*   By: sanghupa <sanghupa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/08 17:01:26 by sanghupa          #+#    #+#             */
-/*   Updated: 2024/01/30 18:43:11 by minakim          ###   ########.fr       */
+/*   Updated: 2024/02/01 22:53:10 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minirt.h"
 
+/// @note tmp
+# define DISTANCE	5
+
 int	get_color(void);
 
-void	camera_up(t_vars *vars, t_container *img)
+void	debug_camera(void)
 {
 	t_camera	*cam;
 	cam = single_cam();
-	cam->lookfrom. // <<
+	printf("\n	[camera]\n");
+	printf("Aspect Ratio: %f\n", cam->aspect_ratio);
+	printf("Image Width: %d\n", cam->image_width);
+	printf("Image Height: %d\n", cam->image_height);
+	printf("Samples Per Pixel: %d\n", cam->samples_per_pixel);
+	printf("Max Depth: %d\n", cam->max_depth);
+	printf("Background Color: (%f, %f, %f)\n", cam->background.x, cam->background.y, cam->background.z);
+	printf("Vertical FOV: %f\n", cam->vfov);
+	printf("Look From: (%f, %f, %f)\n", cam->lookfrom.x, cam->lookfrom.y, cam->lookfrom.z);
+	printf("Look At: (%f, %f, %f)\n", cam->lookat.x, cam->lookat.y, cam->lookat.z);
+	printf("Vup: (%f, %f, %f)\n", cam->vup.x, cam->vup.y, cam->vup.z);
+	printf("Defocus Angle: %f\n", cam->defocus_angle);
+	printf("Focus Distance: %f\n", cam->focus_dist);
+	printf("Center: (%f, %f, %f)\n", cam->center.x, cam->center.y, cam->center.z);
+	printf("U: (%f, %f, %f)\n", cam->u.x, cam->u.y, cam->u.z);
+	printf("V: (%f, %f, %f)\n", cam->v.x, cam->v.y, cam->v.z);
+	printf("W: (%f, %f, %f)\n", cam->w.x, cam->w.y, cam->w.z);
+	printf("Viewport U: (%f, %f, %f)\n", cam->viewport_u.x, cam->viewport_u.y, cam->viewport_u.z);
+	printf("Viewport V: (%f, %f, %f)\n", cam->viewport_v.x, cam->viewport_v.y, cam->viewport_v.z);
+	printf("Pixel Delta U: (%f, %f, %f)\n", cam->pixel_delta_u.x, cam->pixel_delta_u.y, cam->pixel_delta_u.z);
+	printf("Pixel Delta V: (%f, %f, %f)\n", cam->pixel_delta_v.x, cam->pixel_delta_v.y, cam->pixel_delta_v.z);
+	printf("Pixel 00 Location: (%f, %f, %f)\n", cam->pixel00_loc.x, cam->pixel00_loc.y, cam->pixel00_loc.z);
+	printf("Defocus Disk U: (%f, %f, %f)\n", cam->defocus_disk_u.x, cam->defocus_disk_u.y, cam->defocus_disk_u.z);
+	printf("Defocus Disk V: (%f, %f, %f)\n", cam->defocus_disk_v.x, cam->defocus_disk_v.y, cam->defocus_disk_v.z);
+	printf("\n\n");
+
+}
+
+void	camera_up(void)
+{
+	t_camera	*cam;
+	t_vec3		new_lookfrom;
 	
-	mlx_destroy_image(vars->mlx, img->img);
+	cam = single_cam();
+	
+	new_lookfrom = init_vector(cam->lookfrom.x + (DISTANCE / 4), cam->lookfrom.y + DISTANCE, cam->lookfrom.z + (DISTANCE / 4));
+//	new_lookfrom = init_vector(cam->lookfrom.x, cam->lookfrom.y + DISTANCE, cam->lookfrom.z);
+	
+	setup_camera(cam->vfov, new_lookfrom, cam->lookat, (t_vec3){0, 1, 0});
+	debug_camera();
 	render();
 }
 
-void	camera_down(void);
-void	camera_right(void);
-void	camera_left(void);
+void	camera_down(void)
+{
+	t_camera	*cam;
+	t_vec3		new_lookfrom;
+	
+	cam = single_cam();
+	
+	new_lookfrom = init_vector(cam->lookfrom.x, cam->lookfrom.y - DISTANCE, cam->lookfrom.z);
+	setup_camera(cam->vfov, new_lookfrom, cam->lookat, (t_vec3){0, 1, 0});
+	debug_camera();
+	render();
+}
+
+void	camera_right(void)
+{
+	t_camera	*cam;
+	t_vec3		new_lookfrom;
+	
+	cam = single_cam();
+	
+	new_lookfrom = init_vector(cam->lookfrom.x + DISTANCE, cam->lookfrom.y, cam->lookfrom.z);
+	setup_camera(cam->vfov, new_lookfrom, cam->lookat, (t_vec3){0, 1, 0});
+	debug_camera();
+	render();
+}
+
+void	camera_left(void)
+{
+	t_camera	*cam;
+	t_vec3		new_lookfrom;
+//	t_vec3		new_lookat;
+	
+	cam = single_cam();
+	
+	new_lookfrom = init_vector(cam->lookfrom.x - DISTANCE, cam->lookfrom.y, cam->lookfrom.z);
+	
+//	new_lookfrom = init_vector(cam->lookfrom.x - DISTANCE, cam->lookfrom.y, cam->lookfrom.z + DISTANCE / 2 );
+//	new_lookat = init_vector(cam->lookat.x - 10, cam->lookat.y, cam->lookat.z);
+	setup_camera(cam->vfov, new_lookfrom, cam->lookat, (t_vec3){0, 1, 0});
+	debug_camera();
+	render();
+}
+
+void	camera_control(int keycode)
+{
+	else if (keycode == K_AR_U)
+	{
+		camera_up();
+		printf("camera: up\n");
+	}
+	else if (keycode == K_AR_D)
+	{
+		camera_down();
+		printf("camera: down\n");
+	}
+	else if (keycode == K_AR_R)
+	{
+		camera_right();
+		printf("camera: right\n");
+	}
+	else if (keycode == K_AR_L)
+	{
+		camera_left();
+		printf("camera: left\n");
+	}
+}
 
 int	key_hooks(int keycode, t_container *img)
 {
@@ -43,18 +146,23 @@ int	key_hooks(int keycode, t_container *img)
 		temp->color = get_color();
 	else if (keycode == K_AR_U)
 	{
-		if (temp->size < img->w / 4)
-			temp->size++;
+		camera_up();
+		printf("camera: up\n");
 	}
 	else if (keycode == K_AR_D)
 	{
-		if (temp->size > 10)
-			temp->size--;
+		camera_down();
+		printf("camera: down\n");
 	}
-	else if (keycode == K_W)
+	else if (keycode == K_AR_R)
 	{
-		camera_up(vars, img);
-		printf("w\n");
+		camera_right();
+		printf("camera: right\n");
+	}
+	else if (keycode == K_AR_L)
+	{
+		camera_left();
+		printf("camera: left\n");
 	}
 	mlx_put_image_to_window(vars->mlx, vars->win, img->img, 0, 0);
 	return (0);
