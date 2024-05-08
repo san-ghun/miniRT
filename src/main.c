9386d3f00b39dc938dc24dae3c2644bddd374858
minakim <minakim@student.42berlin.de>
2024-05-08 15:12:34 +0200

    update: check one more before PR.

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: sanghupa <sanghupa@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/02 14:22:52 by sanghupa          #+#    #+#             */
-/*   Updated: 2024/05/07 20:52:03 by minakim          ###   ########.fr       */
+/*   Updated: 2024/05/08 15:11:22 by minakim          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,16 +19,17 @@ static t_camera	*cam_ready(t_camera *cam, t_dotrt *rt)
 	cam->background = init_vector(0, 0, 0);
 	cam->defocus_angle = 0.0;
 	cam->focus_dist = 10.0;
-	setup_camera(rt->c->value1, rt->c->point, rt->c->vector, (t_vec3){0, 1, 0});
+	setup_camera(rt->c->value1, rt->c->point, rt->c->vector, \
+	(t_vec3){0, 1, 0});
 	return (cam);
 }
 
 static void	mlx_run(t_vars *vars, t_container *container)
 {
-	mlx_put_image_to_window(vars->mlx, vars->win, container->img, 0, 0);
+	mlx_put_image_to_window(vars->mlx, vars->win, container->img, \
+	0, 0);
 	mlx_hook(vars->win, 17, 0, close_mlx, container);
 	mlx_key_hook(vars->win, key_hooks, container);
-//	mlx_mouse_hook(vars->win, mouse_hooks, container);
 	mlx_loop(vars->mlx);
 }
 
